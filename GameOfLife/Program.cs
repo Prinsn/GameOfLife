b1998e95ccccd0a6fcdc728a292e1c2749f0f1c6
Prinsn <millejef@mail.gvsu.edu>
2019-06-28 10:19:34 -0400

    Refactored code out of Program, corrected inverted time calculation

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,65 +1,24 @@
-﻿using GameOfLife.Engines;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using GameOfLife._Game;
+using GameOfLife.Engines;
+using System.Threading.Tasks;
 
 namespace GameOfLife
 {
     class Program
     {
-        public static int AvgSample = 10;
-        public static Queue<double> FrameTimes = new Queue<double>(AvgSample);
+
         static void Main(string[] args)
         {
-            var board = new Board(10, 10);
-            board.Init(Board._RandomState, new DefaultEngine());
 
-            Console.SetWindowSize(80, 80);
-            Console.SetBufferSize(80, 80);
+            var width = 80;
+            var height = 80;
 
-            Console.CursorVisible = false;
+            var soup = new Board(width, height);
+            soup.Init(Board._RandomState, new DefaultEngine());
 
-            //Life is automatic and eternal
-            var lastUpdate = DateTime.Now;
-            while (true)
-            {
-                Console.SetCursorPosition(0, 0);
-                Console.Write(board.ToString());
-                board.UpdateBoard();
-                RefreshDelay(ref lastUpdate);                
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="last"></param>
-        /// <param name="refreshRate">Refresh rate in miliseconds</param>
-        static void RefreshDelay(ref DateTime last, int refreshRate = 100)
-        {
-            var now = DateTime.Now;
-            var elapsed = last.Millisecond - now.Millisecond;
-            last = now;
-            if(FrameTimes.Count == AvgSample)
-            {
-                FrameTimes.Dequeue();
-            }
-
-            if (elapsed < refreshRate)
-            {
-                Thread.Sleep(refreshRate - elapsed);
-                FrameTimes.Enqueue(0);
-            }            
-            else
-            {
-                FrameTimes.Enqueue(elapsed);
-            }
-
-            Console.WriteLine($"\nFrame rate: {refreshRate} ms");
-            var avg = FrameTimes.Sum() / FrameTimes.Count;
-            Console.WriteLine($"Average Delay: {avg} ms".PadRight(50));
-        }
+            var driver = new ConsoleGame();
+            driver.InitConsole(width, height);
+            driver.LightningBolt(soup);
+        }        
     }
 }
---
+++ GameOfLife/_Game/ConsoleGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameOfLife._Game
+{
+    public class ConsoleGame
+    {
+        private const int _frameSampleCount = 10;
+        private int Generation = 0;
+        public Queue<double> FrameTimes = new Queue<double>(_frameSampleCount);
+
+        public void LightningBolt(Board board)
+        {
+            //Life is automatic and eternal
+            var lastUpdate = DateTime.Now;
+            while (true)
+            {
+                PrintBoard(board);
+                board.UpdateBoard();
+                RefreshDelay(ref lastUpdate);
+            }
+        }
+
+        public void PrintBoard(Board board)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(board.ToString());
+        }
+
+        public void InitConsole(int w, int h)
+        {
+            try
+            {
+                Console.WindowWidth = Console.BufferWidth = w + 5;
+                Console.WindowHeight = Console.BufferHeight = h + 5;
+            }
+            catch
+            {
+                //Just default to max size if any sizes are exceeded
+                Console.WindowWidth = Console.BufferWidth = Console.LargestWindowWidth;
+                Console.WindowHeight = Console.BufferHeight = Console.LargestWindowHeight;
+            }
+
+            Console.CursorVisible = false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="last"></param>
+        /// <param name="refreshRate">Refresh rate in miliseconds</param>
+        public void RefreshDelay(ref DateTime last, int refreshRate = 100)
+        {
+            var now = DateTime.Now;
+            var elapsed = (int)(now.Ticks - last.Ticks) / 10000;
+            var ms = (int)(now.Millisecond - last.Millisecond);
+            if (elapsed < 0 || ms < 0)
+            {
+                elapsed += 0;
+            }
+
+            last = now;
+            if (FrameTimes.Count == _frameSampleCount)
+            {
+                FrameTimes.Dequeue();
+            }
+
+            FrameTimes.Enqueue(elapsed);
+
+            if (elapsed < refreshRate)
+            {
+                Thread.Sleep(refreshRate - elapsed);
+            }
+
+            Console.WriteLine("");
+
+            var avg = Math.Round(FrameTimes.Sum() / FrameTimes.Count);
+            var maxFps = Math.Round(1/(refreshRate /1000.0));            
+            var fps = Math.Round(1.0/(avg/1000.0));
+            Console.WriteLine($"FPS: {Math.Min(fps, maxFps)}/ {maxFps}".PadRight(5));            
+            Console.WriteLine($"Generation: {Generation++}");            
+            //Console.WriteLine($"Actual Framerate Delay: {avg} ms".PadRight(50));
+        }
+    }
+}
